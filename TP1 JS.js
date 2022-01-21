149428c6576c4922dd7df9c496f5acee59631b63
Merwan Barkallah <merwan.barkallah@gmail.com>
2022-01-21 12:22:00 +0100

    TP1 et TP2 JS

---
+++ TP1 JS.js
@@ -0,0 +1,11 @@
+
+let number1;
+let number2;
+let result; 
+result = number1 + number2;
+alert('Bienvenue sur la page de formulaire')
+
+console.log(number1 + " et " + number2); 
+console.log("Le resultat est " + result)
+
+
